5a941255ced543fbe9e72bd6cb8dba8b9acc4f0d
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-11-20 00:30:20 +0900

    Update api fetch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def signup
-    @user = User.new(user_params)
+    @user = User.new(
+      email: params[:email]
+      password: params[:password]
+      password_confirmation: params[:password_confirmation]
+    )
     if @user.save
       session[:user_id] = @user.id
     end
@@ -8,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def login
-    @user = User.find_by(email: user_params.email)
+    @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(user_params.password)
       session[:user_id] = @user.id
     end
@@ -22,6 +26,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end
--- front/src/lib/api.js
+++ front/src/lib/api.js
@@ -1,27 +1,65 @@
-const baseUrl = 'http://localhost:3001/'
+const baseUrl = 'http://localhost:3001'
+
+export const signUp = props => {
+  const {
+    email,
+    password,
+    passwordConfirmation,
+    setLoggedIn
+  } = props;
+
+  const body = new FormData();
+  body.append('email', email);
+  body.append('password', password);
+  body.append('password_confirmation', passwordConfirmation);
+
+  fetchPost({
+    url: `${baseUrl}/signup`,
+    body: body,
+    successAction: setLoggedIn
+  });
+}
+
+export const login = props => {
+  const {
+    email,
+    password,
+    setLoggedIn
+  } = props;
+
+  const body = new FormData();
+  body.append('email', email);
+  body.append('password', password);
+
+  fetchPost({
+    url: `${baseUrl}/login`,
+    body: body,
+    successAction: setLoggedIn
+  });
+}
+
+export const logout = setLoggedIn => {
+  const body = new FormData();
+
+  fetchPost({
+    url: `${baseUrl}/logout`,
+    body: body,
+    successAction: setLoggedIn
+  });
+}
 
 export const fetchMembers = handleGetMembers => {
-  return fetch(baseUrl + 'members')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson);
-        handleGetMembers(responseJson);
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
-};
+  fetchGet({
+    url: `${baseUrl}/members`,
+    successAction: handleGetMembers
+  });
+}
 
 export const fetchMember = (id, handleGetMember) => {
-  return fetch(baseUrl + 'members/' + id)
-    .then((response) => response.json())
-    .then((responseJson) => {
-      console.log(responseJson);
-      handleGetMember(responseJson);
-    })
-    .catch((error) =>{
-      console.error(error);
-    });
+  fetchGet({
+    url: `${baseUrl}/members/${id}`,
+    successAction: handleGetMember
+  });
 }
 
 export const postMember = props => {
@@ -31,21 +69,64 @@ export const postMember = props => {
     setLatest
   } = props;
 
-  const method = 'POST';
   const body = new FormData();
   body.append('name', name);
   body.append('age', age);
 
-  return fetch(baseUrl + 'members', {
+  fetchPost({
+    url: `${baseUrl}/members`,
+    body: body,
+    successAction: setLatest
+  });
+}
+
+const fetchGet = props => {
+  const {
+    url,
+    successAction,
+    failureAction
+  } = props;
+  console.log(props);
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(responseJson => {
+      console.log(responseJson);
+      if (successAction) {
+        successAction(responseJson);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      if (failureAction) {
+        failureAction();
+      }
+    });
+}
+
+const fetchPost = props => {
+  const {
+    url,
+    body,
+    successAction,
+    failureAction
+  } = props;
+
+  const method = 'POST';
+
+  return fetch(url, {
     method,
     body
   })
     .then(response => response.json())
     .then(responseJson => {
       console.log(responseJson);
-      setLatest(false);
+      successAction(responseJson);
     })
     .catch(error => {
       console.error(error);
+      if (failureAction) {
+        failureAction();
+      }
     });
-};
+}
