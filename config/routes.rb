0ef304dac5b72314d06d2b0789a96465f97c54ca
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-11-19 23:16:10 +0900

    get => post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :members
-  get '/signup', to: 'users#create'
-  get '/login' , to: 'users#login'
-  get '/logout', to: 'users#logout'
+  post '/signup', to: 'users#create'
+  post '/login' , to: 'users#login'
+  post '/logout', to: 'users#logout'
 end
